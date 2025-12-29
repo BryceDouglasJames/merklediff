c27b9afac972ce2017bcd40a79c9367f57d1983c
BryceDouglasJames <13964684+BryceDouglasJames@users.noreply.github.com>
2025-12-29 03:13:06 -0500

    Add TimeFormats variable for flexible timestamp parsing in types package

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 // ColumnType represents the data type of a column.
 type ColumnType int
 
@@ -12,6 +14,21 @@ const (
 	ColumnTypeBytes
 	ColumnTypeTimestamp
 )
+
+// TimeFormats is a list of time formats to try when parsing timestamps.
+var TimeFormats = []string{
+	time.RFC3339,                          // 2006-01-02T15:04:05Z07:00
+	time.RFC3339Nano,                      // 2006-01-02T15:04:05.000000000Z07:00
+	time.RFC1123,                          // Mon, 02 Jan 2006 15:04:05 MST
+	time.RFC1123Z,                         // Mon, 02 Jan 2006 15:04:05 -0700
+	time.RFC850,                           // Monday, 02-Jan-06 15:04:05 MST
+	"2006-01-02",                          // ISO date
+	"2006-01-02 15:04:05",                 // ISO datetime
+	"2006-01-02 15:04:05.000000000",       // ISO datetime with nanoseconds
+	"2006-01-02 15:04:05-07:00",           // ISO datetime with timezone
+	"2006-01-02 15:04:05.000000000-07:00", // ISO datetime with nanoseconds and timezone
+	"2006-01-02 15:04:05.000000000+07:00", // ISO datetime with nanoseconds and timezone
+}
 
 func (t ColumnType) String() string {
 	switch t {
